22ad97eaa23dcc2ed638303196603fb11cece8d8
krishnagopal596 <krishnagopal596@gmail.com>
2025-08-23 00:19:54 -0400

    updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const repoName = 'Linkedin-Portfolio'
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  assetPrefix: isGithubActions ? `/${repoName}/` : undefined,
+  basePath: isGithubActions ? `/${repoName}` : undefined,
   images: {
     unoptimized: true,
     domains: ['media.licdn.com', 'images.unsplash.com'],
